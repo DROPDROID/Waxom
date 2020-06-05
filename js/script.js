64d26b91b04fdf3ab8bbad9f984d33350be7f06f
DROPDROID <57584979+DROPDROID@users.noreply.github.com>
2020-06-05 17:30:43 +0300

    Waxom

---
+++ js/script.js
@@ -0,0 +1,10 @@
+  $(document).ready(function(){
+	$(".slider").slick({
+		dots: true,
+		speed: 1000,
+		initialSlide: 2,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		pauseOnDotsHover: false,
+	});
+});
